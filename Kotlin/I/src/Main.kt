d1442c94e1e3f597aa46f312f00cfb10ffc6ecbf
nomah4 <q@ruza.ru>
2024-04-19 19:35:36 +0700

    kotlin add

---
+++ Kotlin/I/src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   print{"hello, wold!"}
+   print{"hello, wold!"}
+   print{"hello, wold!"}
+   print{"hello, wold!"}
+}
